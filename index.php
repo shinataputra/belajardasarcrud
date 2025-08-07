92f94c3a2290d4bf2e51365d048ddd817dd7f87a
shinataputra <shinata.putra@gmail.com>
2025-08-07 14:16:55 +0700

    update

--- index.php
+++ index.php
@@ -6,16 +6,47 @@
     <title>Document</title>
 </head>
 
-    
-
 <body>
 
+    <!-- Koneksi ke database -->
+    <?php
+    $koneksi = mysqli_connect("localhost", "root", "", "dbsiswa");
+    
+    // cek koneksi
+    if (!$koneksi){
+        die("Koneksi gagal: " . mysqli_connect_error());
+    }
+    ?>
+
+    
+
     <!-- form inputan -->
-    <form action="proses.php" method="post">
+    <form action="" method="post">
         <label>Nama</label>
         <input type="text" name="nama">
         <input type="submit" value="Kirim">
     </form>
+
+    <!-- get data dari form -->
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nama = $_POST['nama'];        
+    }
+
+    // insert data ke database
+    $sql = "INSERT INTO tbsiswa (nama) VALUES ('$nama')";
+
+    if (mysqli_query($koneksi, $sql)) {
+        echo "Data berhasil ditambahkan";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
+    }
+    ?>
+
+    
+    
+
+
        
     
 </body>
