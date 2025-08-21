5bdacb6c22468164350ca753f20a5e15fdc07801
shinataputra <shinata.putra@gmail.com>
2025-08-21 12:55:23 +0700

    update : menyederhanakan scirpt

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
 </head>
 
 <body>
@@ -11,43 +13,38 @@
     <!-- Koneksi ke database -->
     <?php
     $koneksi = mysqli_connect("localhost", "root", "", "dbsiswa");
-    
+
     // cek koneksi
-    if (!$koneksi){
+    if (!$koneksi) {
         die("Koneksi gagal: " . mysqli_connect_error());
     }
     ?>
-
-    
-
-    <!-- form inputan -->
     <form action="" method="post">
         <label>Nama</label>
         <input type="text" name="nama">
+        <label>Kelas</label>
+        <input type="text" name="kelas">
+        <label>Alamat</label>
+        <input type="text" name="alamat">
         <input type="submit" value="Kirim">
     </form>
 
     <!-- get data dari form -->
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nama = $_POST['nama'];        
-    }
+        $nama = $_POST['nama'];
+        $kelas = $_POST['kelas'];
+        $alamat = $_POST['alamat'];
 
-    // insert data ke database
-    $sql = "INSERT INTO tbsiswa (nama) VALUES ('$nama')";
+        // insert data ke database
+        $sql = "INSERT INTO tbsiswa (nama, kelas, alamat) VALUES ('$nama', '$kelas', '$alamat')";
+        $result = mysqli_query($koneksi, $sql);
 
-    if (mysqli_query($koneksi, $sql)) {
-        echo "Data berhasil ditambahkan";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
+        // Tutup koneksi
+        mysqli_close($koneksi);
     }
     ?>
 
-    
-    
-
-
-       
-    
 </body>
+
 </html>
